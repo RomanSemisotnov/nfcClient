8a3d114d6fad9c5318b3e147ead92867ef6a8615
russian_rave <semisotnovroman2018@yandex.ru>
2019-12-16 20:51:11 +0300

    ва

--- resources/views/mozaiqa.blade.php
+++ resources/views/mozaiqa.blade.php
@@ -20,21 +20,20 @@
 <img src="https://static10.tgstat.ru/channels/_0/4c/4c626c10f5e4e769e638c3acf58117e1.jpg"/>
 
 <script>
-    let correct_request_id='<?php echo $correct_request_id?>';
-    let promotionDuration='<?php echo $promotionDuration?>';
+    let correct_request_id = '<?php echo $correct_request_id?>';
+    let promotionDuration = '<?php echo $promotionDuration?>';
 
     setTimeout(() => {
 
         let body = 'isConversion=' + encodeURIComponent('yes');
         let xhr = new XMLHttpRequest();
-        xhr.open("POST", 'https://nfc-u.ru/api/uid/update/'+correct_request_id, true);
+        xhr.open("POST", 'https://nfc-u.ru/api/correctrequest/update/' + correct_request_id, true);
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         xhr.send(body);
 
         let redirectTo = '<?php echo $redirectTo?>';
         window.location.href = redirectTo;
-
-    },promotionDuration * 1000);
+    }, promotionDuration * 1000);
 </script>
 
 </body>
